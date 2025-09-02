5efca88e2d032c180bdae8106bc382e0d44dd8b2
Cauteros974 <pusik6666@ukr.net>
2025-09-02 20:52:39 +0300

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import TradingTable from "./TradingTable";
+import React, { useState } from 'react';
+import TradingTable from './TradingTable';
+import StockDetails from './StockDetails';
 import './App.css';
 
 function App() {
+  const [selectedStock, setSelectedStock] = useState(null);
+
+  const handleStockSelect = (stock) => {
+    setSelectedStock(stock);
+  };
+
   return (
     <div className="App">
-      <TradingTable />
+      <TradingTable onStockSelect={handleStockSelect} />
+      {selectedStock && <StockDetails stock={selectedStock} />}
     </div>
   );
 }

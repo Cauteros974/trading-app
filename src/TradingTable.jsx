63a4697a75e3b484f41229045704ae86ca1feaf5
Cauteros974 <pusik6666@ukr.net>
2025-09-02 20:36:45 +0300

    Update TradingTable.jsx

--- src/TradingTable.jsx
+++ src/TradingTable.jsx
@@ -14,5 +14,25 @@ const generateRandomData = (count) => {
     return data;
   };
 
-  
+  const TraidingTable = () => {
+    const [stocks, setStocks] = useState(() => generateRandomData(50));
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setStocks((prevStocks) => {
+                return prevStocks.map((stock) => {
+                    const change = (Math.random() - 0.5) * 0.5;
+                    const newPrice = Math.max(0, stock.price + change);
+                    const newChange = newPrice - stock.price;
+
+                    return {
+                        ...stock,
+                        price: parseFloat(newPrice.toFixed(2)),
+                        change: newChange,
+                    };
+                });
+            });
+        });
+    }, 500);
+  }
 export default TradingTable;

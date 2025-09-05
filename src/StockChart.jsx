952f7041c6ca00886443125e6b6fc7ff4bd78909
Cauteros974 <pusik6666@ukr.net>
2025-09-05 19:34:34 +0300

    Update StockChart.jsx

--- src/StockChart.jsx
+++ src/StockChart.jsx
@@ -34,5 +34,42 @@ const StockChart = ({ stock }) => {
         socket.addEventListener("open", () => {
             socket.send(JSON.stringify({ type: "subscribe", symbol: stock.ticker }));
         });
-    })
+
+        socket.addEventListener("message", (e) => {
+            const message = JSON.parse(e.data);
+            if (message.type === "trade") {
+                message.data.forEach((tradeData) =>{
+                    setChartData((prevData) =>{
+                        const newData = [
+                            ...prevData,
+                            { time: new Date().toLocaleTimeString(), price: tradeData.p },
+                        ];
+                        return newData.length > 50 ? newData.slice(1) : newData;
+                    });
+                });
+            }
+        });
+
+        return () => {
+            if (socket.readyState === WebSocket.OPEN) {
+                socket.send(JSON.stringify({ type: "unsubscrible", symbol: stock.ticker}));
+            }
+            socket.close();
+        };
+    }, [stock]);
+
+    const data = {
+        labels: chartData.map((item) => item.time),
+        datasets: [
+            {
+                label: "Price",
+                data: chartData.map((item) => item.price),
+                borderColor: "rgb(75, 192, 192)",
+                tension: 0.1,
+                PointRadius: 0,
+            },
+        ],
+    };
+
+    
 }

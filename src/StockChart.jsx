d96e76ae3fdce88cdcd51ba9051a4ad95f79a2e1
Cauteros974 <pusik6666@ukr.net>
2025-09-05 18:31:00 +0300

    Update StockChart.jsx

--- src/StockChart.jsx
+++ src/StockChart.jsx
@@ -1,4 +1,49 @@
-import React, { useState, useEffect} from 'react';
-import { Line } from 'react-chartjs-2';
-import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, Title, Legend } from 'chart.js';
+import React, { useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const API_KEY = "d2tendpr01qr5a72a7b0d2tendpr01qr5a72a7bg";
+
+const StockChart = ({ stock }) => {
+    const [chartData, setChartData] = useState([]);
+
+    useEffect(() => {
+        if(!stock) return;
+
+        const socket = new WebSocket('wss://ws.finnhub.io?token=${API_KEY}');
+
+        socket.addEventListener("message", (event: MessageEvent<string>) => {
+            const message = JSON.parse(event.data);
+            if (message.type === "trade") {
+              const tradeData = message.data[0];
+              setChartData((prevData) => {
+                const newData = [
+                  ...prevData,
+                  { time: new Date().toLocaleTimeString(), price: tradeData.p },
+                ];
+                return newData.length > 50 ? newData.slice(1) : newData;
+              });
+            }
+          });
+          
+    });
+}

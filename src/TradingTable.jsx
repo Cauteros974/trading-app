1e52d80de4bce34155395adaff1fcc73ac28ecad
Cauteros974 <pusik6666@ukr.net>
2025-09-05 16:51:26 +0300

    Update TradingTable.jsx

--- src/TradingTable.jsx
+++ src/TradingTable.jsx
@@ -30,7 +30,7 @@ const Row = ({ index, style, data }) => {
 };
 
 const TradingTable = ({ onStockSelect }) => {
-  const [stocks, setStocks] = useState([]); 
+  const [stocks, setStocks] = useState([]);
 
   useEffect(() => {
     const socket = new WebSocket(`wss://ws.finnhub.io?token=${d2rj7v9r01qv11les8i0d2rj7v9r01qv11les8ig}`);
@@ -40,5 +40,46 @@ const TradingTable = ({ onStockSelect }) => {
       tickers.forEach(ticker => {
         socket.send(JSON.stringify({'type': 'subscribe', 'symbol': ticker}));
       });
-  });
-}
+    });
+
+    socket.addEventListener('message', (event) => {
+      const message = JSON.parse(event.data);
+      if (message.type === 'trade') {
+        const tradeData = message.data[0];
+        const { s: symbol, p: price } = tradeData;
+
+        setStocks(prevStocks => {
+          const newStocks = [...prevStocks];
+          let updated = false;
+
+          const updatedStocks = newStocks.map(stock => {
+            if (stock.ticker === symbol) {
+              const change = price - stock.price;
+              updated = true;
+              return {
+                ...stock,
+                price: parseFloat(price.toFixed(2)),
+                change: parseFloat(change.toFixed(2)),
+              };
+            }
+            return stock;
+          });
+
+          if (!updated) {
+            updatedStocks.push({
+              id: symbol,
+              ticker: symbol,
+              price: parseFloat(price.toFixed(2)),
+              change: 0
+            });
+          }
+          updatedStocks.sort((a, b) => a.ticker.localeCompare(b.ticker));
+          return updatedStocks;
+        });
+      }
+    });
+
+    return () => {
+      socket.close();
+    };
+  }, []);

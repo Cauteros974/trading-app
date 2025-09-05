7f556aeb7402a8fb667e8110390284136477ae52
Cauteros974 <pusik6666@ukr.net>
2025-09-05 18:32:22 +0300

    Update StockChart.jsx

--- src/StockChart.jsx
+++ src/StockChart.jsx
@@ -19,31 +19,7 @@ ChartJS.register(
   Title,
   Tooltip,
   Legend
-);
+); 
 
 const API_KEY = "d2tendpr01qr5a72a7b0d2tendpr01qr5a72a7bg";
 
-const StockChart = ({ stock }) => {
-    const [chartData, setChartData] = useState([]);
-
-    useEffect(() => {
-        if(!stock) return;
-
-        const socket = new WebSocket('wss://ws.finnhub.io?token=${API_KEY}');
-
-        socket.addEventListener("message", (event: MessageEvent<string>) => {
-            const message = JSON.parse(event.data);
-            if (message.type === "trade") {
-              const tradeData = message.data[0];
-              setChartData((prevData) => {
-                const newData = [
-                  ...prevData,
-                  { time: new Date().toLocaleTimeString(), price: tradeData.p },
-                ];
-                return newData.length > 50 ? newData.slice(1) : newData;
-              });
-            }
-          });
-          
-    });
-}

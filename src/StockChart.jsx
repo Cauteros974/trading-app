bf5792ee90b4f44a74c97b119694350fab5e684c
Cauteros974 <pusik6666@ukr.net>
2025-09-05 18:20:21 +0300

    Create StockChart.jsx

---
+++ src/StockChart.jsx
@@ -0,0 +1,4 @@
+import React, { useState, useEffect} from 'react';
+import { Line } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, Title, Legend } from 'chart.js';
+

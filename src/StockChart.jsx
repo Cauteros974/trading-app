01bf8a8fbc8b00389db4cf7ac0b4836c96d21c46
Cauteros974 <pusik6666@ukr.net>
2025-09-05 18:45:55 +0300

    Update StockChart.jsx

--- src/StockChart.jsx
+++ src/StockChart.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, use } from "react";
 import { Line } from "react-chartjs-2";
 import {
   Chart as ChartJS,
@@ -23,3 +23,16 @@ ChartJS.register(
 
 const API_KEY = "d2tendpr01qr5a72a7b0d2tendpr01qr5a72a7bg";
 
+const StockChart = ({ stock }) => {
+    const [chartData, setChartData] = useState([]);
+
+    useEffect( () => {
+        if (!stock) return;
+
+        const socket = new WebSocket('wss://ws.finnhub.io?token=${API_KEY}');
+
+        socket.addEventListener("open", () => {
+            socket.send(JSON.stringify({ type: "subscribe", symbol: stock.ticker }));
+        });
+    })
+}

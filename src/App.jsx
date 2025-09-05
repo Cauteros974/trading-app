8b0bb26a768297998d6a4ec0f43d98793658c6c9
Cauteros974 <pusik6666@ukr.net>
2025-09-05 18:11:01 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TradingTable from './TradingTable';
 import StockDetails from './StockDetails';
+import { useTheme } from './ThemeContext';
 import './App.css';
 
 function App() {
   const [selectedStock, setSelectedStock] = useState(null);
+  const [theme, toggleTheme] = useTheme;
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  })
 
   const handleStockSelect = (stock) => {
     setSelectedStock(stock);

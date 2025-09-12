2864df340c50e8d2a8dc039887172701b77e0dd4
Cauteros974 <pusik6666@ukr.net>
2025-09-12 19:56:34 +0300

    Update Account.jsx

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -9,6 +9,21 @@ const Account = ({ session }) => {
           alert(error.message);
         }
     };
+
+    return(
+        <motion.div
+            className="account-container"
+            initial={{ opacity: 0, scale: 0,9 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+        >
+            <h2>Personal Account</h2>
+            <p> Welcome, **{session.user.email}**!</p>
+            <button onClick={handleLogout} className="logout-button">
+                Exit
+            </button>
+        </motion.div>
+    )
 };
 
 export default Account;

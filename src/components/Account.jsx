78e1745bf23e268cdf9ef5dc27d1a844115775eb
Cauteros974 <pusik6666@ukr.net>
2025-09-14 18:29:14 +0300

    commit

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,29 +1,29 @@
-import React from "react";
-import { supabase } from "../../supabaseClient";
-import { motion } from "framer-motion";
+import React from 'react';
+import { supabase } from '../../supabaseClient';
+import { motion } from 'framer-motion';
 
 const Account = ({ session }) => {
-    const handleLogout = async () => {
-        const { error } = await supabase.auth.signOut();
-        if (error) {
-          alert(error.message);
-        }
-    };
+  const handleLogout = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      alert(error.message);
+    }
+  };
 
-    return(
-        <motion.div
-            className="account-container"
-            initial={{ opacity: 0, scale: 0.9 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.5 }}
-        >
-            <h2>Personal Account</h2>
-            <p> Welcome, **{session.user.email}**!</p>
-            <button onClick={handleLogout} className="logout-button">
-                Exit
-            </button>
-        </motion.div>
-    );
+  return (
+    <motion.div
+      className="account-container"
+      initial={{ opacity: 0, scale: 0.9 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      <h2>Personal account</h2>
+      <p>Welcome, **{session.user.email}**!</p>
+      <button onClick={handleLogout} className="logout-button">
+        LogOut
+      </button>
+    </motion.div>
+  );
 };
 
 export default Account;
